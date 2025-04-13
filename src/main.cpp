e13a66ff13a6e645a06c88bbccde0b1dad04163d
Danielius Marciukovas <danielmarchukov@gmail.com>
2025-04-13 11:54:11 +0100

    Added Restclient CPP with Gtest, clang-format and clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
-#include <boost/multiprecision/cpp_int.hpp>
+#include "restclient-cpp/connection.h"
+#include "restclient-cpp/restclient.h"
 #include <iostream>
 #include <ql/quantlib.hpp>
 
-using namespace boost::multiprecision;
-
 int main(int argc, char *argv[]) {
-  long long num1 = 1523844560192817464;
-  long long num2 = 598274671729184766;
-  int128_t result = (int128_t)num1 * num2;
-  std::cout << "The product of the two integers is:" << "\n" << result << "\n";
-  QuantLib::Option::Type OptionType(QuantLib::Option::Call);
-  std::cout << "Option Type = " << OptionType << std::endl;
-  return 0;
+    RestClient::init();
+    RestClient::Connection *conn =
+        new RestClient::Connection("https://paper-api.alpaca.markets");
+    std::cout << conn->get("/v2/account").body;
+    return 0;
 }
