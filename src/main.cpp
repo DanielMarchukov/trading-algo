0072b168fb11e33b80eaedd90f6d2f17c6433c26
Danielius Marciukovas <danielmarchukov@gmail.com>
2025-05-27 21:39:39 +0100

    Address build issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,4 @@
-#include "restclient-cpp/connection.h"
-#include "restclient-cpp/restclient.h"
 #include <cstdlib>
-#include <iostream>
 #include <ql/quantlib.hpp>
 
-int main(int argc, char *argv[]) {
-    RestClient::init();
-    RestClient::Connection *conn =
-        new RestClient::Connection("https://paper-api.alpaca.markets");
-    RestClient::HeaderFields headers;
-    headers["APCA-API-KEY-ID"] = std::getenv("APCA_API_KEY_ID");
-    headers["APCA-API-SECRET-KEY"] = std::getenv("APCA_API_SECRET_KEY");
-    conn->SetHeaders(headers);
-    std::cout << conn->get("/v2/account").body;
-    RestClient::disable();
-    delete conn;
-    return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
