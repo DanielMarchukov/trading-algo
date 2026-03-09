d9a5c3dd196b69f52d4c906d74cab95deb9d28ba
DanielMarchukov <danielmarchukov@gmail.com>
2026-03-09 09:47:37 +0000

    [65] Fix clang-format 19 incompatibility in timespec init

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -17,7 +17,7 @@
 
 [[nodiscard]] inline uint64_t nowNanos() noexcept {
 #if defined(__linux__) || defined(__gnu_linux__)
-  struct timespec ts{};
+  struct timespec ts = {};
   clock_gettime(CLOCK_MONOTONIC, &ts);
   return static_cast<uint64_t>(ts.tv_sec) * 1'000'000'000ULL +
          static_cast<uint64_t>(ts.tv_nsec);
